5fcc7cd4bbba4fa0b42e4e6ae3ca1683838cbedb
Bernd Beuster <bernd.beuster@gmail.com>
2021-03-14 10:19:17 +0100

    Rename helper function

--- src/Board.hs
+++ src/Board.hs
@@ -7,10 +7,11 @@ type Direction = (Int, Int) -- (dy, dx)
 newtype Board = Board (Array Position Char)
 
 instance Show Board where
-  show (Board brd) = unlines $ go $ elems brd
+  show (Board brd) = unlines $ rows $ elems brd
     where
-      go xs | null xs   = []
-            | otherwise = first : go rest
+      rows :: String -> [String]
+      rows xs | null xs   = []
+              | otherwise = first : rows rest
         where
           (_, (_, cols)) = bounds brd
           (first, rest)  = splitAt cols xs
