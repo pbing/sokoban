b658107022ba9d6f4997d15b1dc41ff07b12e7e5
Bernd Beuster <bernd.beuster@gmail.com>
2021-03-14 11:53:03 +0100

    Use list comprehension for Show(Board)

--- src/Board.hs
+++ src/Board.hs
@@ -4,14 +4,14 @@ import Data.Array
 
 type Position  = (Int, Int) -- (y, x)
 type Direction = (Int, Int) -- (dy, dx)
+
 newtype Board = Board (Array Position Char)
 
 instance Show Board where
-  show (Board brd) = unlines $ rows $ elems brd
-    where
-      rows :: String -> [String]
-      rows xs | null xs   = []
-              | otherwise = first : rows rest
-        where
-          (_, (_, cols)) = bounds brd
-          (first, rest)  = splitAt cols xs
+  show = unlines . boardRows
+      
+boardRows :: Board -> [String]
+boardRows (Board brd) = [row r | r <- [rmin..rmax]]
+  where
+    ((rmin, cmin), (rmax, cmax)) = bounds brd
+    row r = [brd ! (r, c) | c <- [cmin..cmax]]
